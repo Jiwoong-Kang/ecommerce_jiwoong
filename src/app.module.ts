04ebdb2c666ae72dae6fe7c82ea755aa02698c50
Jiwoong <dnd0311@naver.com>
2024-08-01 17:27:19 +0900

    connect database with docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule, OrderModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'ecommerce_user',
+      password: 'ecommerce_password',
+      database: 'ecommerce_db',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    ProductModule,
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
