c48c4d8c1d0b647c8e0dc493e88da32a51f464d4
Jiwoong <dnd0311@naver.com>
2024-08-06 14:30:17 +0900

    create and get products

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  // product 전체를 불러오는 api
+  @Get('/all')
+  async getAllProducts() {
+    return await this.productService.getProducts();
+  }
+
+  // product를 등록하는 api
+  @Post('/create')
+  async registerProduct(@Body() createProductDto: CreateProductDto) {
+    return await this.productService.postProduct(createProductDto);
+  }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async getProducts() {
+    const products = await this.productRepository.find();
+    return { count: products.length, data: products };
+  }
+
+  async postProduct(createProductDto: CreateProductDto) {
+    const newProduct = await this.productRepository.create(createProductDto);
+    await this.productRepository.save(newProduct);
+    return newProduct;
+  }
+}
