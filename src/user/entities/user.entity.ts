aeaf5f159ac6b247774dc4da73dbefc1a244aad9
Jiwoong <dnd0311@naver.com>
2024-08-06 16:34:20 +0900

    add password hash and profile image

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,5 +1,8 @@
 import { BaseEntity } from '../../common/base.entity';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import * as gravatar from 'gravatar';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @Entity()
 export class User extends BaseEntity {
@@ -12,4 +15,26 @@ export class User extends BaseEntity {
 
   @Column()
   public password: string;
+
+  @Column()
+  public profileImg?: string;
+  // ?는 있어도 그만 없어도 그만이라는 뜻이다. optional
+  @BeforeInsert()
+  async beforeSaveFunction() {
+    try {
+      // password 암호화
+      const saltValue = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, saltValue);
+
+      //profile 이미지 자동생성
+      this.profileImg = gravatar.url(this.email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+        protocol: 'https',
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
 }
