0d850d3d15ed6f919e826b46e424971a764c8fc9
Jiwoong <dnd0311@naver.com>
2024-08-09 16:00:10 +0900

    jwt inspect

---
+++ src/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayload } from '../interfaces/tokenPayload.interface';
+import { UserService } from '../../user/user.service';
+//jwt자체를 검증하는 단계이다
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      // 토큰 위치를 헤드에 놓겠다. 베어러토큰방식이다
+      secretOrKey: configService.get('ACCESS_TOKEN_SECRET'),
+    });
+  }
+  async validate(payload: TokenPayload) {
+    return await this.userService.getUserById(payload.userId);
+  }
+}
