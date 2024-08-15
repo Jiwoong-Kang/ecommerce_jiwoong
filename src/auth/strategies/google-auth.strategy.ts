ac64a4afced33d93ac271d16e102e92e06f0491f
Jiwoong <dnd0311@naver.com>
2024-08-15 10:29:06 +0900

    Google Login #1

---
+++ src/auth/strategies/google-auth.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth2';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class GoogleAuthStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      clientID: configService.get('GOOGLE_AUTH_CLIENT_ID'),
+      clientSecret: configService.get('GOOGLE_AUTH_CLIENT_SECRET'),
+      callbackURL: configService.get('GOOGLE_AUTH_CALLBACK_URL'),
+      scope: ['profile', 'email'],
+    });
+  }
+
+  async validate(
+    _accessToken: string,
+    _refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ) {
+    return profile;
+  }
+}
